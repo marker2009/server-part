3f86e12b737aff82b716fdff426b4de4cf1bad31
Your Name <you@example.com>
2024-02-13 15:45:05 +0300

    flask  start work

--- hom.py
+++ hom.py
@@ -1,14 +1,42 @@
-a = int(input())
-ol = 0
-m = []
-for i in range(a):
-    buf = int(input())
-    if buf % 6 == 0 and buf % 10 == 4:
-        ol += 1
-        m.append(buf)
-if ol == 0:
-    print("НЕТ")
-else:
-    print(ol)
-    for i in range(ol):
-        print(m[i])
+import time
+
+
+class bd:
+    def __init__(self):
+        self.users = ["mark", "admin"]
+        self.passwords = ["1", "123ghkijf"]
+
+    def check_user(self, user, password):
+        for i, el in enumerate(self.users):
+            if el == user and self.passwords[i] == password:
+                return [True, i]
+        return [False, -1]
+
+
+bd1 = bd()
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/members')
+def hello_world():
+    rstequest
+    return {'mark': "topssss"}
+
+
+@app.route('/get_settings', methods=["POST", "GET"])
+def hello_world1():
+    time.sleep(1)
+    user = str(request.json['user'])
+    password = str(request.json['password'])
+    print(request.json, request.method, [user, password])
+    print(bd1.check_user(user, password))
+    buf = bd1.check_user(user, password)
+    if buf[0] == True:
+        return {"status": "ok", "id": buf[1]}
+    else:
+        return {"status": "bad"}
+
+
+app.run(debug=True)
--- xfg.py
+++ xfg.py
@@ -1,4 +1,11 @@
-import dill
+def f(x):
+    return 300 - (300 / x + 1 ) * (x - 10)
+mi = [1000000, 0]
+for i in range(1, 1000000):
+    buf = i / 10000
+    mi = min(mi, [abs(f(buf)), buf])
 
-while True:
-    dill.dump_session(filename)
+    if f(buf) == 0:
+        print(buf)
+print(mi)
+print(f(60))
